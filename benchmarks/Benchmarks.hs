26fa7b5f9aafae5a07c5a8bee97846ca83ff8a09
Bryan O'Sullivan <bos@serpentine.com>
2013-05-01 16:52:28 -0700

    Benchmark sorted, random, and reversed fromList
    --HG--
    extra : amend_source : 22ca8a033557418bd8294ac15249815bd744a1c3

--- benchmarks/Benchmarks.hs
+++ benchmarks/Benchmarks.hs
@@ -6,24 +6,42 @@ import Control.Exception (evaluate)
 import Control.Monad.Trans (liftIO)
 import Criterion.Config (defaultConfig)
 import Criterion.Main (bench, bgroup, defaultMainWith, whnf)
+import System.Random.MWC (asGenST, uniformR, withSystemRandom)
 import qualified Data.ByteString.Char8 as B
 import qualified Data.CritBit.Map.Lazy as C
 import qualified Data.HashMap.Lazy as H
 import qualified Data.Map as Map
+import qualified Data.Vector as V
+import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Unboxed as U
+
+every k = go 0
+  where
+    go i (x:xs)
+        | i == k-1  = x : go 0 xs
+        | otherwise = go (i+1) xs
+    go _ _ = []
 
 main = do
-  keys <- B.lines <$> B.readFile "/usr/share/dict/words"
-  let kvs = zip keys [(0::Int)..]
+  ordKeys <- (every 5 . B.lines) <$> B.readFile "/usr/share/dict/words"
+  let ordKVs = zip ordKeys [(0::Int)..]
+      revKVs = reverse ordKVs
+  randKVs <- withSystemRandom . asGenST $ \gen ->
+             let kvVec = V.fromList ordKVs
+             in (G.toList . G.backpermute kvVec) <$>
+                G.replicateM (G.length kvVec)
+                             (uniformR (0, G.length kvVec - 1) gen)
+  let fromList kvs = [
+          bench "critbit" $ whnf C.fromList kvs
+        , bench "map" $ whnf Map.fromList kvs
+        , bench "hashmap" $ whnf H.fromList kvs
+        ]
   defaultMainWith
     defaultConfig
-    (liftIO . evaluate $ rnf [kvs])
-    [ bgroup "critbit" [
-        bench "fromList" $ whnf C.fromList kvs
-      ]
-    , bgroup "map" [
-        bench "fromList" $ whnf Map.fromList kvs
-      ]
-    , bgroup "hashmap" [
-        bench "fromList" $ whnf H.fromList kvs
+    (liftIO . evaluate $ rnf [ordKVs, randKVs, revKVs])
+    [ bgroup "fromList" [
+        bgroup "ordered" $ fromList ordKVs
+      , bgroup "random" $ fromList randKVs
+      , bgroup "reversed" $ fromList revKVs
       ]
     ]
