8b5c7a1882439e8b1160f16f9a0b8f9df44d5997
Bryan O'Sullivan <bos@serpentine.com>
2013-05-01 10:01:11 -0700

    Add an Eq instance

--- Data/CritBit/Types/Internal.hs
+++ Data/CritBit/Types/Internal.hs
@@ -16,9 +16,17 @@ data Node k v = Empty
                 }
                 deriving (Show)
 
+instance (Eq k, Eq v) => Eq (Node k v) where
+    Empty        == Empty       = True
+    Leaf k0 v0   == Leaf k1 v1  = k0 == k1 && v0 == v1
+    i0@(Internal _ _ _ _)  == i1@(Internal _ _ _ _) =
+        ibyte i0 == ibyte i1 && iotherBits i0 == iotherBits i1 &&
+        ileft i0 == ileft i1 && iright i0 == iright i1
+    _            == _           = False
+
 newtype CritBit k v = CritBit {
       cbRoot :: Node k v
-    } deriving (Show)
+    } deriving (Show, Eq)
 
 class (Eq k) => CritBitKey k where
     byteCount :: k -> Int
