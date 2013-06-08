haskell/critbit

commit b8e028a4af13bde844a051e26938b4e1412bb284

Correct difference/\intersection type signatures.

Data/CritBit/Tree.hs
@@ -545,7 +545,7 @@ unionsWith f cs = List.foldl' (unionWith f) empty cs
 -- > let l = fromList [("a", 5), ("b", 3)]
 -- > let r = fromList [("A", 2), ("b", 7)]
 -- > difference l r == fromList [("a", 5)]
-difference :: (CritBitKey k) => CritBit k v -> CritBit k v -> CritBit k v
+difference :: (CritBitKey k) => CritBit k v -> CritBit k w -> CritBit k v
 difference a b = differenceWithKey (\_ _ _ -> Nothing) a b
 {-# INLINEABLE difference #-}
 
@@ -559,8 +559,8 @@ difference a b = differenceWithKey (\_ _ _ -> Nothing) a b
 -- > let l = fromList [(pack "a", 5), (pack "b", 3), (pack "c", 8)]
 -- > let r = fromList [(pack "a", 2), (pack "b", 7), (pack "d", 8)]
 -- > differenceWith f l r == fromList [(pack "b", 10), (pack "c", 8)]
-differenceWith :: (CritBitKey k) => (v -> v -> Maybe v)
-                 -> CritBit k v -> CritBit k v -> CritBit k v
+differenceWith :: (CritBitKey k) => (v -> w -> Maybe v)
+                 -> CritBit k v -> CritBit k w -> CritBit k v
 differenceWith f a b = differenceWithKey (const f) a b
 {-# INLINEABLE differenceWith #-}
 
@@ -574,8 +574,8 @@ differenceWith f a b = differenceWithKey (const f) a b
 -- > let l = fromList [("a", 5), ("b", 3), ("c", 8)]
 -- > let r = fromList [("a", 2), ("b", 7), ("d", 8)]
 -- > differenceWithKey f l r == fromList [("b", 11), ("c", 8)]
-differenceWithKey :: (CritBitKey k) => (k -> v -> v -> Maybe v)
-                    -> CritBit k v -> CritBit k v -> CritBit k v
+differenceWithKey :: (CritBitKey k) => (k -> v -> w -> Maybe v)
+                    -> CritBit k v -> CritBit k w -> CritBit k v
 differenceWithKey = binarySetOpWithKey id
 {-# INLINEABLE differenceWithKey #-}
 
@@ -585,7 +585,7 @@ differenceWithKey = binarySetOpWithKey id
 -- > let l = fromList [("a", 5), ("b", 3)]
 -- > let r = fromList [("A", 2), ("b", 7)]
 -- > intersection l r == fromList [("b", 3)]
-intersection :: (CritBitKey k) => CritBit k v -> CritBit k v -> CritBit k v
+intersection :: (CritBitKey k) => CritBit k v -> CritBit k w -> CritBit k v
 intersection a b = intersectionWithKey (\_ x _ -> x) a b
 {-# INLINEABLE intersection #-}
 
@@ -594,8 +594,8 @@ intersection a b = intersectionWithKey (\_ x _ -> x) a b
 -- > let l = fromList [("a", 5), ("b", 3)]
 -- > let r = fromList [("A", 2), ("b", 7)]
 -- > intersectionWith (+) l r == fromList [("b", 10)]
-intersectionWith :: (CritBitKey k) => (v -> v -> v)
-                 -> CritBit k v -> CritBit k v -> CritBit k v
+intersectionWith :: (CritBitKey k) => (v -> w -> x)
+                 -> CritBit k v -> CritBit k w -> CritBit k x
 intersectionWith f a b = intersectionWithKey (const f) a b
 {-# INLINEABLE intersectionWith #-}
 
@@ -605,19 +605,19 @@ intersectionWith f a b = intersectionWithKey (const f) a b
 -- > let l = fromList [("a", 5), ("b", 3)]
 -- > let r = fromList [("A", 2), ("b", 7)]
 -- > intersectionWithKey f l r == fromList [("b", 11)]
-intersectionWithKey :: (CritBitKey k) => (k -> v -> v -> v)
-                    -> CritBit k v -> CritBit k v -> CritBit k v
+intersectionWithKey :: (CritBitKey k) => (k -> v -> w -> x)
+                    -> CritBit k v -> CritBit k w -> CritBit k x
 intersectionWithKey f = binarySetOpWithKey (const Empty) f'
   where
     f' k v1 v2 = Just (f k v1 v2)
 
 -- | Performs binary set operation on two maps
 binarySetOpWithKey :: (CritBitKey k)
-    => (Node k v -> Node k v) -- ^ Process unmatched node in first map
-    -> (k -> v -> v -> Maybe v) -- ^ Process matching values
+    => (Node k v -> Node k x) -- ^ Process unmatched node in first map
+    -> (k -> v -> w -> Maybe x) -- ^ Process matching values
     -> CritBit k v -- ^ First map
-    -> CritBit k v -- ^ Second map
-    -> CritBit k v
+    -> CritBit k w -- ^ Second map
+    -> CritBit k x
 binarySetOpWithKey left both (CritBit lt) (CritBit rt) = CritBit $ top lt rt
   where
     -- Assumes that empty nodes exist only on the top level

commit 8ddc94a1ff82c6590cd9c723942d88633ec6eb8d

Type error introduced by commit `b8e028a4` fixed

Data/CritBit/Tree.hs
@@ -752,7 +752,7 @@ binarySetOpWithKey left both (CritBit lt) (CritBit rt) = CritBit $ top lt rt
 
 -- | Detects wether branch in 'Internal' node comes 'before' or 'after'
 --   branch initiated by 'Leaf'
-leafBranch :: CritBitKey k => Node k v -> Node k v -> k -> t -> t -> t
+leafBranch :: CritBitKey k => Node k v -> Node k w -> k -> t -> t -> t
 leafBranch (Leaf lk _) (Internal _ _ sbyte sbits) sk before after
     | dbyte > sbyte || dbyte == sbyte && dbits >= sbits = before
     | otherwise                                         = after
@@ -762,8 +762,8 @@ leafBranch _ _ _ _ _ = error "Data.CritBit.Tree.leafBranch: unpossible"
 {-# INLINE leafBranch #-}
 
 -- | Select child to link under node 'n' by 'k'
-switch :: (CritBitKey k) => k -> Node k v -> Node k v -> Node k v
-       -> Node k v -> Node k v -> Node k v
+switch :: (CritBitKey k) => k -> Node k v -> Node k w -> Node k w
+       -> Node k w -> Node k w -> Node k w
 switch k n a0 b0 a1 b1
   | direction k n == 0 = link n a0 b0
   | otherwise          = link n a1 b1
@@ -779,9 +779,9 @@ minKey n = leftmost
 -- | Links childs to the parent node
 link :: (CritBitKey k)
      => Node k v -- ^ parent
-     -> Node k v -- ^ left child
-     -> Node k v -- ^ right child
-     -> Node k v
+     -> Node k w -- ^ left child
+     -> Node k w -- ^ right child
+     -> Node k w
 link _ Empty b = b
 link _ a Empty = a
 link (Internal _ _ byte bits) a b = Internal a b byte bits