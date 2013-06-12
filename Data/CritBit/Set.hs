ec552fbb18ce88050458abb84404140e1e7224cf
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2013-06-13 00:00:51 +0400

    Lifts replaced by wraps

--- Data/CritBit/Set.hs
+++ Data/CritBit/Set.hs
@@ -156,7 +156,7 @@ elems = toList
 -- > toList (fromList ["b", "a"]) == ["a", "b"]
 -- > toList empty == []
 toList :: Set a -> [a]
-toList = liftS T.keys
+toList = wrapS id T.keys
 {-# INLINABLE toList #-}
 
 -- | /O(n)/. The number of elements in the set.
@@ -165,7 +165,7 @@ toList = liftS T.keys
 -- > size (singleton "a")            == 1
 -- > size (fromList ["a", "c", "b"]) == 3
 size :: Set a -> Int
-size = liftS T.size
+size = wrapS id T.size
 {-# INLINABLE size #-}
 
 -- | /O(k)/. Is the element in the set?
@@ -194,7 +194,7 @@ notMember a (Set s) = T.notMember a s
 -- > lookupLT "aa" (fromList ["a", "b"]) == Just "a"
 -- > lookupLT "a"  (fromList ["a", "b"]) == Nothing
 lookupLT :: (CritBitKey a) => a -> Set a -> Maybe a
-lookupLT = (fmap fst .) . liftVS T.lookupLT
+lookupLT = wrapVS (fmap fst) T.lookupLT
 {-# INLINABLE lookupLT #-}
 
 -- | /O(k)/. Find smallest element greater than the given one.
@@ -203,7 +203,7 @@ lookupLT = (fmap fst .) . liftVS T.lookupLT
 -- > lookupGT "aa" (fromList ["a", "b"]) == Just "b"
 -- > lookupGT "a"  (fromList ["a", "b"]) == Just "b"
 lookupGT :: (CritBitKey a) => a -> Set a -> Maybe a
-lookupGT = (fmap fst .) . liftVS T.lookupGT
+lookupGT = wrapVS (fmap fst) T.lookupGT
 {-# INLINABLE lookupGT #-}
 
 -- | /O(k)/. Find lagest element smaller than or equal to the given one.
@@ -213,7 +213,7 @@ lookupGT = (fmap fst .) . liftVS T.lookupGT
 -- > lookupGE "a"  (fromList ["a", "b"]) == Just "a"
 -- > lookupGE ""   (fromList ["a", "b"]) == Nothing
 lookupLE :: (CritBitKey a) => a -> Set a -> Maybe a
-lookupLE = (fmap fst .) . liftVS T.lookupLE
+lookupLE = wrapVS (fmap fst) T.lookupLE
 {-# INLINABLE lookupLE #-}
 
 -- | /O(k)/. Find smallest element greater than or equal to the given one.
@@ -222,37 +222,37 @@ lookupLE = (fmap fst .) . liftVS T.lookupLE
 -- > lookupGE "b"  (fromList ["a", "b"]) == Just "b"
 -- > lookupGE "bb" (fromList ["a", "b"]) == Nothing
 lookupGE :: (CritBitKey a) => a -> Set a -> Maybe a
-lookupGE = (fmap fst .) . liftVS T.lookupGE
+lookupGE = wrapVS (fmap fst) T.lookupGE
 {-# INLINABLE lookupGE #-}
 
 -- | /O(n+m)/. Is this a subset?
 -- @(s1 `isSubsetOf` s2)@ tells whether @s1@ is a subset of @s2@.
 isSubsetOf :: (CritBitKey a) => Set a -> Set a -> Bool
-isSubsetOf = liftSS T.isSubmapOf
+isSubsetOf = wrapSS id T.isSubmapOf
 {-# INLINABLE isSubsetOf #-}
 
 -- | /O(n+m)/. Is this a proper subset (ie. a subset but not equal)?
 -- @(s1 `isSubsetOf` s2)@ tells whether @s1@ is a proper subset of @s2@.
 isProperSubsetOf :: (CritBitKey a) => Set a -> Set a -> Bool
-isProperSubsetOf = liftSS T.isProperSubmapOf
+isProperSubsetOf = wrapSS id T.isProperSubmapOf
 {-# INLINABLE isProperSubsetOf #-}
 
 -- | /O(k)/. Insert an element in a set.
 -- If the set already contains an element equal to the given value,
 -- it is replaced with the new value.
 insert :: (CritBitKey a) => a -> Set a -> Set a
-insert = (Set .) . liftVS (flip T.insert ())
+insert = wrapVS Set (flip T.insert ())
 {-# INLINABLE insert #-}
 
 -- | /O(log n)/. Delete an element from a set.
 delete :: (CritBitKey a) => a -> Set a -> Set a
-delete = (Set .) . liftVS T.delete
+delete = wrapVS Set T.delete
 {-# INLINABLE delete #-}
 
 -- | /O(K)/. The union of two sets, preferring the first set when
 -- equal elements are encountered.
 union :: (CritBitKey a) => Set a -> Set a -> Set a
-union = (Set .) . liftSS T.union
+union = wrapSS Set T.union
 {-# INLINABLE union #-}
 
 -- | The union of a list of sets: (@'unions' == 'foldl' 'union' 'empty'@).
@@ -262,13 +262,13 @@ unions = List.foldl' union empty
 
 -- | /O(K)/. The difference of two sets.
 difference :: (CritBitKey a) => Set a -> Set a -> Set a
-difference = (Set .) . liftSS T.union
+difference = wrapSS Set T.union
 {-# INLINABLE difference #-}
 
 -- | /O(K)/. The intersection of two sets. Elements of the
 -- result come from the first set.
 intersection :: (CritBitKey a) => Set a -> Set a -> Set a
-intersection = (Set .) . liftSS T.union
+intersection = wrapSS Set T.union
 {-# INLINABLE intersection #-}
 
 -- | /O(n)/. Filter all elements that satisfy the predicate.
@@ -277,14 +277,14 @@ intersection = (Set .) . liftSS T.union
 -- > filter (> "x") (fromList ["a", "b"]) == empty
 -- > filter (< "a") (fromList ["a", "b"]) == empty
 filter :: (a -> Bool) -> Set a -> Set a
-filter = (Set .) . liftVS (T.filterWithKey . (const .))
+filter = wrapVS Set (T.filterWithKey . (const .))
 {-# INLINABLE filter #-}
 
 -- | /O(n)/. Partition the set into two sets, one with all elements that satisfy
 -- the predicate and one with all elements that don't satisfy the predicate.
 -- See also 'split'.
 partition :: (CritBitKey a) => (a -> Bool) -> Set a -> (Set a, Set a)
-partition = ((Set *** Set) .) . liftVS (T.partitionWithKey . (const .))
+partition = wrapVS (Set *** Set) (T.partitionWithKey . (const .))
 {-# INLINABLE partition #-}
 
 -- | /O(k)/. The expression (@'split' x set@) is a pair @(set1,set2)@
@@ -297,7 +297,7 @@ partition = ((Set *** Set) .) . liftVS (T.partitionWithKey . (const .))
 -- > split "d" (fromList ["b", "d"]) == (singleton "b", empty)
 -- > split "e" (fromList ["b", "d"]) == (fromList ["b", "d"], empty)
 split :: (CritBitKey a) => a -> Set a -> (Set a, Set a)
-split = ((Set *** Set) .) . liftVS T.split
+split = wrapVS (Set *** Set) T.split
 {-# INLINABLE split #-}
 
 -- | /O(log n)/. Performs a 'split' but also returns whether the pivot
@@ -309,7 +309,7 @@ split = ((Set *** Set) .) . liftVS T.split
 -- > splitMember "d" (fromList ["b", "d"]) == (singleton "b", True, empty)
 -- > splitMember "e" (fromList ["b", "d"]) == (fromList ["b", "d"], False, empty)
 splitMember :: (CritBitKey a) => a -> Set a -> (Set a, Bool, Set a)
-splitMember = (pack .) . liftVS T.splitLookup
+splitMember = wrapVS pack T.splitLookup
   where pack (l, m, r) = (Set l, isJust m, Set r)
 {-# INLINABLE splitMember #-}
 
@@ -319,7 +319,7 @@ splitMember = (pack .) . liftVS T.splitLookup
 -- It's worth noting that the size of the result may be smaller if,
 -- for some @(x,y)@, @x \/= y && f x == f y@
 map :: (CritBitKey a2) => (a1 -> a2) -> Set a1 -> Set a2
-map = (Set .) . liftVS T.mapKeys
+map = wrapVS Set T.mapKeys
 {-# INLINABLE map #-}
 
 -- | /O(n)/. The @'mapMonotonic' f s == 'map' f s@, but works only when
@@ -332,7 +332,7 @@ map = (Set .) . liftVS T.mapKeys
 -- >     where ls = toList s
 mapMonotonic :: (CritBitKey a2) => (a1 -> a2) -> Set a1 -> Set a2
 mapMonotonic = error "Depends on T.mapKeysMonotonic"
---mapMonotonic = (Set .) . liftVS T.mapKeysMonotonic
+--mapMonotonic = wrapVS Set T.mapKeysMonotonic
 {-# INLINABLE mapMonotonic #-}
 
 -- | /O(n)/. Fold the elements in the set using the given left-associative
@@ -342,14 +342,14 @@ mapMonotonic = error "Depends on T.mapKeysMonotonic"
 --
 -- > toDescList set = foldl (flip (:)) [] set
 foldl :: (a -> b -> a) -> a -> Set b -> a
-foldl f = liftVS (T.foldlWithKey ((const .) . f))
+foldl f = wrapVS id (T.foldlWithKey ((const .) . f))
 {-# INLINE foldl #-}
 
 -- | /O(n)/. A strict version of 'foldl'. Each application of the operator is
 -- evaluated before using the result in the next application. This
 -- function is strict in the starting value.
 foldl' :: (a -> b -> a) -> a -> Set b -> a
-foldl' f = liftVS (T.foldlWithKey' ((const .) . f))
+foldl' f = wrapVS id (T.foldlWithKey' ((const .) . f))
 {-# INLINE foldl' #-}
 
 -- | /O(n)/. Fold the elements in the set using the given right-associative
@@ -359,60 +359,60 @@ foldl' f = liftVS (T.foldlWithKey' ((const .) . f))
 --
 -- > toAscList set = foldr (:) [] set
 foldr :: (a -> b -> b) -> b -> Set a -> b
-foldr f = liftVS (T.foldrWithKey (const . f))
+foldr f = wrapVS id (T.foldrWithKey (const . f))
 {-# INLINE foldr #-}
 
 -- | /O(n)/. A strict version of 'foldr'. Each application of the operator is
 -- evaluated before using the result in the next application. This
 -- function is strict in the starting value.
 foldr' :: (a -> b -> b) -> b -> Set a -> b
-foldr' f = liftVS (T.foldrWithKey' (const . f))
+foldr' f = wrapVS id (T.foldrWithKey' (const . f))
 {-# INLINE foldr' #-}
 
 -- | /O(k)/. The minimal element of a set.
 findMin :: Set a -> a
-findMin = fst . liftS T.findMin
+findMin = wrapS fst T.findMin
 {-# INLINE findMin #-}
 
 -- | /O(k)/. The maximal element of a set.
 findMax :: Set a -> a
-findMax = fst . liftS T.findMax
+findMax = wrapS fst T.findMax
 {-# INLINE findMax #-}
 
 -- | /O(log n)/. Delete the minimal element. Returns an empty set if the set is empty.
 deleteMin :: Set a -> Set a
-deleteMin = Set . liftS T.deleteMin
+deleteMin = wrapS Set T.deleteMin
 {-# INLINE deleteMin #-}
 
 -- | /O(log n)/. Delete the maximal element. Returns an empty set if the set is empty.
 deleteMax :: Set a -> Set a
-deleteMax = Set . liftS T.deleteMax
+deleteMax = wrapS Set T.deleteMax
 {-# INLINE deleteMax #-}
 
 -- | /O(log n)/. Delete and find the minimal element.
 --
 -- > deleteFindMin set = (findMin set, deleteMin set)
 deleteFindMin :: Set a -> (a, Set a)
-deleteFindMin = (fst *** Set) . liftS T.deleteFindMin
+deleteFindMin = wrapS (fst *** Set) T.deleteFindMin
 {-# INLINE deleteFindMin #-}
 
 -- | /O(log n)/. Delete and find the maximal element.
 --
 -- > deleteFindMax set = (findMax set, deleteMax set)
 deleteFindMax :: Set a -> (a, Set a)
-deleteFindMax = (fst *** Set) . liftS T.deleteFindMax
+deleteFindMax = wrapS (fst *** Set) T.deleteFindMax
 {-# INLINE deleteFindMax #-}
 
 -- | /O(log n)/. Retrieves the minimal key of the set, and the set
 -- stripped of that element, or 'Nothing' if passed an empty set.
 minView :: Set a -> Maybe (a, Set a)
-minView = fmap (fst *** Set) . liftS T.minViewWithKey
+minView = wrapS (fmap (fst *** Set)) T.minViewWithKey
 {-# INLINE minView #-}
 
 -- | /O(log n)/. Retrieves the maximal key of the set, and the set
 -- stripped of that element, or 'Nothing' if passed an empty set.
 maxView :: Set a -> Maybe (a, Set a)
-maxView = fmap (fst *** Set) . liftS T.maxViewWithKey
+maxView = wrapS (fmap (fst *** Set)) T.maxViewWithKey
 {-# INLINE maxView #-}
 
 -- | /O(n)/. Convert the set to an ascending list of elements.
@@ -433,20 +433,20 @@ fromAscList = liftFromList T.fromAscList
 fromDistinctAscList :: (CritBitKey a) => [a] -> Set a
 fromDistinctAscList = liftFromList T.fromDistinctAscList
 
--- | Lifts tree operation to set operation
-liftS :: (CritBit a () -> r) -> Set a -> r
-liftS f (Set s) = f s
-{-# INLINE liftS #-}
+-- | Wraps tree operation to set operation
+wrapS :: (r -> q) -> (CritBit a () -> r) -> Set a -> q
+wrapS f g (Set s) = f $ g s
+{-# INLINE wrapS #-}
 
--- | Lifts (value, tree) operation to (value, set) operation
-liftVS :: (t -> CritBit a () -> r) -> t -> Set a -> r
-liftVS = (liftS .)
-{-# INLINE liftVS #-}
+-- | Wraps (value, tree) operation to (value, set) operation
+wrapVS :: (r -> q) -> (t -> CritBit a () -> r) -> t -> Set a -> q
+wrapVS f g a (Set s) = f $ g a s
+{-# INLINE wrapVS #-}
 
--- | Lifts (tree, tree) operation to (set, set) operation
-liftSS :: (CritBit a () -> CritBit a () -> r) -> Set a -> Set a -> r
-liftSS = liftS . (liftS .)
-{-# INLINE liftSS #-}
+-- | Wraps (tree, tree) operation to (set, set) operation
+wrapSS :: (r -> q) -> (CritBit a () -> CritBit a () -> r) -> Set a -> Set a -> q
+wrapSS f g (Set s1) (Set s2) = f $ g s1 s1
+{-# INLINE wrapSS #-}
 
 liftFromList :: ([(a, ())] -> CritBit a ()) -> [a] -> Set a
 liftFromList f xs = Set . f . zip xs . repeat $ ()
